f23027c4866a48806b327846ac93b5875004a561
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-12-16 11:10:15 +0100

    add auth

---
+++ app/routes/auth.callback.tsx
@@ -0,0 +1,35 @@
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
+import { createServerClient, parse, serialize } from "@supabase/ssr";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+  const next = requestUrl.searchParams.get("next") || "/";
+  const headers = new Headers();
+  if (code) {
+    const cookies = parse(request.headers.get("Cookie") ?? "");
+    const supabase = createServerClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(key) {
+            return cookies[key];
+          },
+          set(key, value, options) {
+            headers.append("Set-Cookie", serialize(key, value, options));
+          },
+          remove(key, options) {
+            headers.append("Set-Cookie", serialize(key, "", options));
+          },
+        },
+      }
+    );
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return redirect(next, { headers });
+    }
+  }
+  // return the user to an error page with instructions
+  return redirect("/login", { headers });
+};
